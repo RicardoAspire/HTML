e489435a052eff046260d52a5b2cfd81acff44d5
RicardoAspire <ricardo.flores@aspiresys.com>
2022-04-07 19:53:14 -0500

    Websockets, ServerSentEvents and LocalStorage DONE

--- scripts.js
+++ scripts.js
@@ -1,4 +1,32 @@
 /* FORM VALIDATION */
+
+// Array of cities
+const cities = ["Puebla", "Guadalajara", "CDMX","Monterrey","Sinaloa","Tlaxcala"];
+const genders = ["Male", "Female", "Other"];
+
+//Take the element from the DOM 
+var select = document.getElementById('city');
+var selectGender = document.getElementById('gender');
+
+//For each item in my cities array I will add that option
+for (var i = 0; i<cities.length; i++){
+    var opt = document.createElement('option');
+    opt.value = cities[i];
+    opt.innerHTML = cities[i];
+    select.appendChild(opt);
+}
+for (var i = 0; i<genders.length; i++){
+  var inp = document.createElement('input');
+  inp.setAttribute('type','radio');
+  inp.setAttribute('name','gender');
+  inp.setAttribute('value',genders[i]);
+  inp.setAttribute('id',genders[i]);
+  var inpTwo = document.createElement('label');
+  inpTwo.innerHTML = genders[i];
+  selectGender.appendChild(inp);
+  selectGender.appendChild(inpTwo);
+}
+
 const form  = document.getElementsByTagName('form')[0];
 const email = document.getElementById('mail');
 const emailError = document.querySelector('#mail + span.error');
@@ -20,26 +48,13 @@ form.addEventListener('submit', function (event) {
 });
 
 function showError() {
-  if(email.validity.valueMissing) {
-    emailError.textContent = 'You need to enter an e-mail valid format.';
-  } else if(email.validity.typeMismatch) {
-    emailError.textContent = 'Entered value needs to be an e-mail valid format.';
-  } else if(email.validity.tooShort) {
-    emailError.textContent = `Email should be at least ${ email.minLength } characters; you entered ${ email.value.length }.`;
+  if(email.validity.valueMissing || email.validity.typeMismatch || email.validity.tooShort) {
+    emailError.textContent = `You need to enter an e-mail valid format with at least ${email.minLength} characters.`;
   }
   emailError.className = 'error active';
 } 
 
 /* WEB COMPONENTS AND CUSTOM ELEMENTS */
-class MyProduct extends HTMLElement{
-    constructor(){
-        super();
-        this.attachShadow({mode: 'open'})
-        this.shadowRoot.innerHTML = '<style>h2 {color:blue}</style><h2>Hi! I am a web componnent!</h2>';
-    }
-}
-window.customElements.define('my-product', MyProduct);
-
 class helloWorld extends HTMLElement{
     constructor(){
         super();
@@ -50,7 +65,6 @@ class helloWorld extends HTMLElement{
         return ['name',"surname"];
     }
     attributeChangedCallback(nameAtr, oldValue, newValue){
-        console.log("Llegue al switch")
         switch(nameAtr){
             case "name": 
                 this.name = newValue; 
@@ -61,13 +75,12 @@ class helloWorld extends HTMLElement{
         }
     }
     connectedCallback(){
-        console.log("Llegue");
-
-        this.innerHTML = `<div>
-            <h1>Hello ${this.name} ${this.surname} from a callback function with an attribute changed call back</h1>
-            <h1>This is a paragraph</h1>
-        </div>`;
-        this.style.color = "green";
+        const select = document.getElementById('tag');
+        var message = document.createElement('h1');
+        message.innerHTML = `Hello ${this.name} ${this.surname} from a Web Component`;
+        message.style.color="#DF01D7";
+        select.appendChild(message);
     }
 }
 window.customElements.define("hello-world", helloWorld);
+
---
+++ server-sent-events/index.js
@@ -0,0 +1,24 @@
+const app = require("express")();
+
+app.get("/", (req, res) => res.send("Hello!"));
+
+app.get("/stream", (req, res)=>{
+    res.setHeader("Content-Type","text/event-stream");
+    send(res)
+});
+
+let i = 0; 
+function send (res){
+    res.write("data: "+`Hello! ${i++}\n\n`);
+    setTimeout(() => send(res),1000);
+}
+
+app.listen(8080);
+console.log("Listen to 8080");
+
+/* Commands to start listening for the messages
+    In the explorer console 
+    1.- let sse = new EventSource("http://localhost:8080/stream");
+    2.- sse.onmessage = console.log
+
+*/
