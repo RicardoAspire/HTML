e8bbeac5dd2e68d316aa328d540121f5bebfd708
RicardoAspire <ricardo.flores@aspiresys.com>
2022-04-12 12:43:47 -0500

    HTML and CSS blocks Complete

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 /* FORM VALIDATION */
 
 // Array of cities
@@ -98,7 +99,7 @@ ws.addEventListener("message", ({ data })=> {
         reader.readAsText(data);
         reader.onload = () => {
             const socketDOM = document.getElementById('webSocket');
-            var socketMessage = document.createElement('h2');
+            var socketMessage = document.createElement('p');
             socketMessage.innerHTML = `${reader.result}`;
             socketDOM.appendChild(socketMessage);
         };
@@ -200,7 +201,7 @@ function touch(event){
 
   /* DEVICE ORIENTATION */
   const deviceOrientation = document.getElementById("deviceOrientation");
-  var orientationMessage = document.createElement('h2');
+  var orientationMessage = document.createElement('p');
 
   window.addEventListener('deviceorientation', function(){
     var angle = window.orientation;
@@ -214,3 +215,63 @@ function touch(event){
     }
     deviceOrientation.appendChild(orientationMessage);
   },true);
+
+  /* EDITABLE CONTENT */
+  const editable = document.getElementById("editable");
+  const editableButton = document.getElementById("editableButton");
+  const uneditableButton = document.getElementById("uneditableButton");
+  
+  editableButton.onclick = function(){
+    editable.contentEditable = true;
+  }
+  uneditableButton.onclick = function(){
+    editable.contentEditable = false;
+  }
+  
+  /* DRAG AND DROP */
+  const box = document.querySelector('#box');
+  const bigBox = document.querySelector('#bigBox');
+  /* 
+  box.addEventListener('dragstart', e=>{
+    console.log('Drag start');
+  });
+  box.addEventListener('dragend', e=>{
+    console.log('Drag end');
+  });
+  box.addEventListener('drag', e=>{
+    console.log('Drag');
+  }); 
+  */
+  bigBox.addEventListener('dragenter', e =>{
+    console.log('drag enter');
+  });
+  bigBox.addEventListener('dragleave', e =>{
+    console.log('drag leave');
+  });
+  bigBox.addEventListener('dragover', e =>{
+    e.preventDefault();
+    console.log('drag over');
+  });
+  bigBox.addEventListener('drop', e =>{
+    console.log('drop');
+    bigBox.appendChild(box);
+  });
+
+  /* WEB WORKERS */
+    const worker = new Worker('worker.js');
+    new Worker(URL.createObjectURL(new Blob(["("+worker_function.toString()+")()"], {type: 'text/javascript'})));
+  
+    const sumButton = document.querySelector("#sumButton");
+    const bgButton = document.querySelector("#bgButton");
+  
+    sumButton.addEventListener("click", (event) => {
+      worker.postMessage('Hello');
+    });
+  
+    bgButton.addEventListener("click", event =>{
+      if(document.body.style.background != "green"){
+        document.body.style.background = "green";
+      }else{
+        document.body.style.background = "blue";
+      }
+    });
